2a157f2c21ac744b64d48a78e8d95856b444cb19
ashiag1990-ship-it <ashiag1990@gmail.com>
2026-09-20 01:58:48 +0000

    Allow embedding in an iframe on nationalmsa.org via CSP frame-ancestors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          // Explicitly allow this app to be embedded in an <iframe> on the
+          // NMSA marketing site (and its Bolt preview domain while testing).
+          // Without this, some hosts/CDNs default to blocking framing
+          // entirely, which shows up in the browser as "refused to connect."
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' https://nationalmsa.org https://*.nationalmsa.org https://bolt.new https://*.bolt.new;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
